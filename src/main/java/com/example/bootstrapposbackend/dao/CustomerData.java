cfe3aa643e3cb78ef2a64e886cc00cc820c0460c
chamika-damith <chamikadamith9@gmail.com>
2024-08-13 11:53:10 +0530

    update customer data

--- src/main/java/com/example/bootstrapposbackend/dao/CustomerData.java
+++ src/main/java/com/example/bootstrapposbackend/dao/CustomerData.java
@@ -1,4 +1,13 @@
 package com.example.bootstrapposbackend.dao;
 
+import com.example.bootstrapposbackend.dto.CustomerDTO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public interface CustomerData {
+    CustomerDTO getCustomer(String cusId, Connection connection) throws SQLException;
+    String saveCustomer(CustomerDTO customerDTO,Connection connection);
+    boolean deleteCustomer(String cusId,Connection connection);
+    boolean updateCustomer(String cusId,CustomerDTO customer,Connection connection);
 }
