261f4b8adb7ede87e643cf1b463bc895432984db
chamika-damith <chamikadamith9@gmail.com>
2024-08-13 11:54:27 +0530

    update customer data process

--- src/main/java/com/example/bootstrapposbackend/dao/impl/CustomerDataProcess.java
+++ src/main/java/com/example/bootstrapposbackend/dao/impl/CustomerDataProcess.java
@@ -1,4 +1,29 @@
 package com.example.bootstrapposbackend.dao.impl;
 
-public class CustomerDataProcess {
+import com.example.bootstrapposbackend.dao.CustomerData;
+import com.example.bootstrapposbackend.dto.CustomerDTO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class CustomerDataProcess implements CustomerData {
+    @Override
+    public CustomerDTO getCustomer(String cusId, Connection connection) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public String saveCustomer(CustomerDTO customerDTO, Connection connection) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteCustomer(String cusId, Connection connection) {
+        return false;
+    }
+
+    @Override
+    public boolean updateCustomer(String cusId, CustomerDTO customer, Connection connection) {
+        return false;
+    }
 }
