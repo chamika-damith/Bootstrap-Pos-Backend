5991bf2faca35a443c7f36dab38ee5e041754967
chamika-damith <chamikadamith9@gmail.com>
2024-08-13 11:56:24 +0530

    get connection from pool

--- src/main/java/com/example/bootstrapposbackend/controller/CustomerController.java
+++ src/main/java/com/example/bootstrapposbackend/controller/CustomerController.java
@@ -1,9 +1,27 @@
 package com.example.bootstrapposbackend.controller;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+import java.sql.Connection;
+
 @WebServlet("/customer")
 public class CustomerController extends HttpServlet {
+    Connection connection;
 
+
+    @Override
+    public void init() throws ServletException {
+
+        try {
+            var ctx = new InitialContext();
+            DataSource pool = (DataSource) ctx.lookup("java:comp/env/jdbc/customers");
+            this.connection = pool.getConnection();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
